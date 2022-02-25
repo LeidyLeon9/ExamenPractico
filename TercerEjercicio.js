90acd1b44c812821c282c3863b20495c423401a2
leidy johanna leon diaz <laion_992@hotmail.com>
2022-02-24 21:05:39 -0500

    Tercer ejercicio

---
+++ TercerEjercicio.js
@@ -0,0 +1,11 @@
+let numero = prompt("Ingrese el numero", 0);
+  
+    if(numero%2==0){
+        
+        console.log("El número "+numero+" es par");
+    
+    }else{
+        
+        console.log("El número "+numero+" es impar");
+        
+    }
